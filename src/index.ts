4ffd66fdb92dd1695b5b3227b56520e87f394e4c
Rajiv Khushiram <rajivkhushiram@hotmail.com>
2024-10-12 23:45:07 +1030

    Return types + type safety on TOMTOM search

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
-import { getPlaceAutocomplete } from './maps-api'
+import { getPlaceAutocomplete } from './maps-api';
+import { Address } from './types/';
 
-export async function getAutoCompleteDetails(address: any): Promise<any> {
-    const apiKey = process.env.TOMTOM_API_KEY;
-    // get autocomplete results
-    const res = getPlaceAutocomplete(process.env.TOMTOM_API_KEY, address).then(async (autocompleteResults) => {
-        const res = []
-        return res
-    })
-    // loop over and get details and map results
-    return res
+export async function getAutoCompleteDetails(
+  address: string
+): Promise<Address[]> {
+  const apiKey = process.env.TOMTOM_API_KEY;
+  if (!apiKey) {
+    throw new Error(
+      'TOMTOM_API_KEY is not defined in the environment variables.'
+    );
+  }
+  // get autocomplete results
+
+  const res: Address[] = await getPlaceAutocomplete(apiKey, address);
+
+  return res;
 }
--- src/maps-api.ts
+++ src/maps-api.ts
@@ -1,16 +1,40 @@
-import axios from 'axios'
+import axios from 'axios';
+import { Address, LocationResult } from './types';
 
 // https://developer.tomtom.com/search-api/documentation/search-service/fuzzy-search
-export async function getPlaceAutocomplete(key: string, address: string) {
-    const autocomplete = await axios.get(`https://api.tomtom.com/search/2/search/${address}.json'`, {
+export async function getPlaceAutocomplete(
+  key: string,
+  address: string
+): Promise<Address[]> {
+  try {
+    if (!address.trim()) {
+      throw Error('Address is required');
+    }
+    const response = await axios.get(
+      `https://api.tomtom.com/search/2/search/${address}.json`,
+      {
         params: {
-            key,
-            limit: 100,
-        }
+          key,
+          limit: 100,
+          countrySet: 'AU',
+        },
+      }
+    );
+
+    const results: Array<LocationResult> = response.data.results;
+
+    return results.map((result: LocationResult): Address => {
+      return {
+        placeId: result.id,
+        streetName: result.address.streetName,
+        streetNumber: result.address.streetNumber,
+        countryCode: result.address.countryCode,
+        country: result.address.country,
+        freeformAddress: result.address.freeformAddress,
+        municipality: result.address.municipality,
+      };
     });
-    return autocomplete.data.results.map((result) => {
-        return {
-            placeId: result.id,
-        }
-    })
+  } catch (error: any) {
+    throw new Error(`Failed to fetch autocomplete results: ${error?.message}`);
+  }
 }
